fa720ec7e666541faf95675ca7cee6323e6c361b
Devin <devins.d@protonmail.com>
2022-08-22 15:22:08 -0300

    index view with app stats method

--- HangboardTrainingAPI/Controllers/HomeController.cs
+++ HangboardTrainingAPI/Controllers/HomeController.cs
@@ -1,11 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Text.Encodings.Web;
+using MyBoardsAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using MyBoardsAPI.Models.ViewModels;
 
 namespace MyBoardsAPI.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly MyBoardsDbContext _db;
+
+    public HomeController(MyBoardsDbContext db)
+    {
+        _db = db;
+    }
+    
+    
     // GET
+    public async Task<IActionResult> Index()
+    {
+        // boards created 
+        var hangboards = await _db.Hangboards.ToListAsync();
+        var boardCount = hangboards.Count();
+        
+        // workouts created 
+        var workouts = await _db.Workouts.ToListAsync();
+        var workoutCount = workouts.Count();
+        
+        // pins placed 
+        var holds = await _db.Holds.ToListAsync();
+        var holdCount = holds.Count();
+
+        var ViewModel = new IndexView()
+        {
+            HangboardCount = boardCount,
+            WorkoutCount = workoutCount,
+            HoldCOunt = holdCount
+        };
+
+        return View("Index", ViewModel);
+    }
+    
     public IActionResult PrivacyPolicy()
     {
         return View("Privacy");
---
+++ HangboardTrainingAPI/Models/ViewModels/IndexView.cs
@@ -0,0 +1,8 @@
+namespace MyBoardsAPI.Models.ViewModels;
+
+public class IndexView
+{
+    public int HangboardCount { get; set; }
+    public int WorkoutCount { get; set; }
+    public int HoldCOunt { get; set; }
+}
