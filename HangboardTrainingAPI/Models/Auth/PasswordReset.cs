4a24702be6e9adc4837dec3ba4d82ff0356158d7
Devin Davis <ddavis@immediac.com>
2022-09-11 08:29:17 -0300

    password reset initial setup complete

---
+++ HangboardTrainingAPI/Models/Auth/PasswordReset.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBoardsAPI.Models.Auth;
+
+[BindProperties]
+public class PasswordReset
+{
+    public string Email { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
