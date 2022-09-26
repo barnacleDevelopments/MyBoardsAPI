4491b59b22a86890f0e3314363de020e8266de7e
Devin Davis <ddavis@immediac.com>
2022-09-26 11:52:13 -0300

    removed website content

--- HangboardTrainingAPI/Models/Feature.cs
+++
@@ -1,9 +0,0 @@
-namespace MyBoardsAPI.Models
-{
-    public class Feature : BaseModel
-    {
-        public string Name { get; set; } = String.Empty;
-        public string Description { get; set; } = String.Empty;
-        public bool IsRoadMap { get; set; }
-    }
-}
